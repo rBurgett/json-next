621191e4ac149407447c3003b8fee302bfc136fb
Ryan <ryan@burgettweb.net>
2016-04-11 16:54:25 -0400

    Create stringify and parse methods

--- src/main.js
+++ src/main.js
@@ -1,15 +1,126 @@
+/* eslint no-unused-vars:1 */
+
 import 'babel-polyfill';
+import _ from 'lodash';
 
-const JsonNext = function() {
+const mapifySetifyByPath = (data, pathArr) => {
 
-    this.stringify = (data) => {
-        return JSON.stringify(data);
-    };
+    let evalStr = 'data';
+    pathArr.forEach(([ type, k ], i) => {
+        console.log(evalStr);
+        if(type === 'm') {
+            if(i === pathArr.length - 1) {
+                eval(`${evalStr} = new Map(${evalStr})`);
+            } else {
+                let innerObj;
 
-    this.parse = (jsonStr) => {
-        return JSON.parse(jsonStr);
-    };
+                const getStr = `${evalStr}.get('${k}')`;
+                eval(`innerObj = ${getStr}`);
+
+                const newObj = mapifySetifyByPath(innerObj, pathArr.slice(i + 1));
+
+                const setStr = `${evalStr}.set('${k}', newObj)`;
+                eval(setStr);
+
+            }
+        } else if(type === 's') {
+            if(i === pathArr.length - 1) {
+                eval(`${evalStr} = new Set(${evalStr})`);
+            } else {
+
+                let setArr;
+                const getSetArrStr = `setArr = [...${evalStr}]`;
+                eval(getSetArrStr);
+
+                setArr[k] = mapifySetifyByPath(setArr[k], pathArr.slice(i + 1));
+
+                const toSetStr = `${evalStr} = new Set(setArr)`;
+                eval(toSetStr);
+
+            }
+        } else {
+            evalStr = evalStr + `['${k}']`;
+            // dataObjToModify = dataObjToModify ? data[k] : dataObjToModify[k];
+        }
+    });
+    return data;
+};
+
+const JsonNext = {
+
+    stringify(data, options = {async: false, pretty: false}) {
+
+        // o, a, m, s
+
+        let msArr = [];
+
+        const recStringify = (item, pathArr = []) => {
+
+            if(_.isArray(item)) {
+                return '[' + item.map((d, i) => recStringify(d, pathArr.concat([ ['a', i] ]))).join(',') + ']';
+            } else if(_.isPlainObject(item)) {
+                return '{' + Object.keys(item).map(k => `"${k}":` + recStringify(item[k], pathArr.concat([ ['o', k] ]))).join(',') + '}';
+            } else if(_.isMap(item)) {
+
+                msArr = msArr.concat([pathArr.concat([ ['m', ''] ])]);
+
+                item.forEach((val, key) => {
+                    if(!_.isString(key)) throw new Error('A Map can only be encoded as JSON if all keys are strings');
+                });
+
+                const arr = [...item];
+
+                return '[' + arr.map(([ key, val ]) => `["${key}",${recStringify(val, pathArr.concat([ ['m', key] ]))}]`).join(',') + ']';
+
+            } else if(_.isSet(item)) {
+
+                msArr = msArr.concat([pathArr.concat([ ['s', ''] ])]);
+
+                const arr = [...item];
+
+                return '[' + arr.map((d, i) => recStringify(d, pathArr.concat([ ['s', i] ]))).join(',') + ']';
+
+            } else {
+                return JSON.stringify(item);
+            }
+        };
+
+        const jsonStr = recStringify(data);
+        const finalJsonStr = `{"json_next_paths":${JSON.stringify(msArr.sort((a, b) => a.length > b.length))},"data":${jsonStr}}`;
+
+        if(options.pretty) {
+            return JSON.stringify(JSON.parse(finalJsonStr), null, '  ');
+        } else {
+            return finalJsonStr;
+        }
+    },
+
+    parse(jsonStr) {
+
+        let origData;
+        try {
+            origData = JSON.parse(jsonStr);
+        } catch(e) {
+            throw new Error(e);
+        }
+
+        if(_.isPlainObject(origData) && _.isArray(origData.json_next_paths)) {
+
+            const data = origData.data;
+            const pathsArr = origData.json_next_paths.concat().sort((a, b) => a.length > b.length);
+
+            // console.log('pathsArr is', JSON.stringify(origData.json_next_paths));
+
+            return pathsArr
+                .reduce((dataObj, pathArr) => {
+                    return mapifySetifyByPath(dataObj, pathArr);
+                }, data);
+
+        } else {
+            return origData;
+        }
+    }
 
 };
 
-export default JsonNext;
+export default Object.create(JsonNext);
--- test/tests.js
+++ test/tests.js
@@ -1,27 +1,98 @@
 /* global describe, it */
 
 import 'should';
-import JsonNext from '../src/main';
-
-const jsonNext = new JsonNext();
+import jsonNext from '../src/main';
+import fs from 'fs';
 
 describe('JsonNext', function() {
+
+    const myMap = new Map([['first', 'Benjamin'], ['last', 'Cisco']]);
+    const mySecondMap = new Map([['first', 'Jake'], ['last', 'Cisco']]);
+    myMap.set('second', mySecondMap);
+    const mySet = new Set(['Benjamin', 'Jake']);
+    const mySecondSet = new Set();
+    mySecondSet.add('me');
+    mySecondSet.add('you');
+    mySet.add(mySecondSet);
+
+    const myObj = {
+        name: 'Benjamin',
+        age: 37,
+        map: myMap,
+        set: mySet
+    };
 
     describe('stringify', function() {
 
         it('should take a JavaScript data object and return a JSON string', () => {
-            jsonNext.stringify({name: 'Ryan', age: 30}).should.equal('{"name":"Ryan","age":30}');
+            jsonNext.stringify({name: 'Ryan', age: 30}).should.be.String();
         });
 
     });
 
     describe('parse', function() {
 
+        const nestedDataObj = {
+            // something: ['one', 'two', 'three'],
+            anything: {
+                // someMap: new Map([
+                //     ['something', new Map([
+                //         ['set1', new Set(['one', 'two', 'three'])]
+                //     ])],
+                //     ['else', new Map([
+                //         ['someSet', new Set(['one', 'two', 'three'])]
+                //     ] )]
+                // ]),
+                someMap: new Map([
+                    [ 'some', new Map([ ['some', 'thing'] ]) ],
+                    // [ 'other', new Map([['some', new Set(['some', 'thing']) ]]) ],
+                    [ 'thing', new Set(['any', 'thing']) ]
+                    // ['another', 'thing']
+                ])
+                // someSet: new Set( [new Set(['Benjamin', 'Jake']), new Set(['Miles', 'Molly'])] )
+            }
+            // nums: [5435, 542543, 542, 54, 325, 42, 54, 325, 42],
+            // arr: [
+            //     [543, 545, 54, 325, 4325, 432, 543, 254, 325, 34],
+            //     [159, 154 ,891, 4891, 18593, 1852, 888],
+            //     [543, 545, 54, 325, 4325, 432, 543, 254, 325, 34],
+            //     {
+            //         something: 'here',
+            //         too: {here: ['is', 'an', {}]}
+            //     }
+            // ]
+        };
+
+        // console.log(nestedDataObj.anything.someMap.get('something'));
+
         it('should take a JSON string and return a JavaScript data object', () => {
-            const data = jsonNext.parse('{"name":"Ryan","age":30}');
+            const jsonStr = jsonNext.stringify(myObj);
+            const data = jsonNext.parse(jsonStr);
             data.should.be.Object();
             data.name.should.be.String();
             data.age.should.be.Number();
+            data.map.get('last').should.be.String();
+            data.map.get('second').get('first').should.be.String();
+            data.set.has('Benjamin').should.be.True();
+            [...[...data.set][2]][1].should.equal('you');
+            const arrDataObj = ['Ryan', 'Hannah'];
+            jsonNext.stringify(arrDataObj).should.equal(JSON.stringify({json_next_paths: [], data: arrDataObj}));
+            const objDataObj = {name: 'Ryan'};
+            jsonNext.stringify(objDataObj).should.equal(JSON.stringify({json_next_paths: [], data: objDataObj}));
+            const dataStr = 'something';
+            jsonNext.stringify(dataStr).should.equal(JSON.stringify({json_next_paths: [], data: dataStr}));
+            const dataNum = 1000;
+            jsonNext.stringify(dataNum).should.equal(JSON.stringify({json_next_paths: [], data: dataNum}));
+            const jsonString = '["one", "two", "three"]';
+            JSON.stringify(JSON.parse(jsonString)).should.equal(JSON.stringify(jsonNext.parse(jsonString)));
+
+            // jsonNext.parse(jsonNext.stringify(nestedDataObj));
+
+            fs.writeFileSync('single.json', jsonNext.stringify(nestedDataObj, {pretty: true}), 'utf8');
+            fs.writeFileSync('multiple.json', jsonNext.stringify(jsonNext.parse(jsonNext.stringify(nestedDataObj)), {pretty: true}), 'utf8');
+
+            jsonNext.stringify(nestedDataObj, {pretty: true}).should.equal(jsonNext.stringify(jsonNext.parse(jsonNext.stringify(nestedDataObj)), {pretty: true}));
+
         });
 
     });
